1f2e08dba40ee50dcd52d4b89551d7c1f8ede2f5
SUGAE <setayesh.qsmi@gmail.com>
2022-01-05 20:06:16 +0330

    addExpert in ExpertDao

---
+++ src/main/java/dao/ExpertDao.java
@@ -0,0 +1,21 @@
+package dao;
+
+import model.Users.Expert;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+
+public class ExpertDao {
+    static SessionFactory sessionFactory = new Configuration().configure().buildSessionFactory();
+
+    public void addExpert(Expert expert) {
+        Session session = sessionFactory.openSession();
+        Transaction transaction = session.beginTransaction();
+        session.save(expert);
+        transaction.commit();
+        session.close();
+    }
+
+
+}
