c30433d24a5fe7ce6f21c9595e9d3e0bf58778cd
SUGAE <setayesh.qsmi@gmail.com>
2022-01-05 20:50:34 +0330

    new ExpertDao in CustomerService

--- src/main/java/service/CustomerService.java
+++ src/main/java/service/CustomerService.java
@@ -1,13 +1,16 @@
 package service;
 
 import dao.CustomerDao;
+import dao.ExpertDao;
 import model.Users.Customer;
 
 public class CustomerService {
     CustomerDao customerDao = new CustomerDao();
+    ExpertDao expertDao = new ExpertDao();
 
     public void createCustomer(String firstName, String lastName, String address, String email, String password) {
         Customer customer = new Customer(firstName, lastName, address, email, password);
         customerDao.addCustomer(customer);
     }
+
 }
