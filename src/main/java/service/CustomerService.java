16bc44ecba4bc7ccd31f5f2b3d8cb7a6d1841882
SUGAE <setayesh.qsmi@gmail.com>
2022-01-05 20:47:23 +0330

    add createCustomer in CustomerService

--- src/main/java/service/CustomerService.java
+++ src/main/java/service/CustomerService.java
@@ -1,4 +1,13 @@
 package service;
 
+import dao.CustomerDao;
+import model.Users.Customer;
+
 public class CustomerService {
+    CustomerDao customerDao = new CustomerDao();
+
+    public void createCustomer(String firstName, String lastName, String address, String email, String password) {
+        Customer customer = new Customer(firstName, lastName, address, email, password);
+        customerDao.addCustomer(customer);
+    }
 }
