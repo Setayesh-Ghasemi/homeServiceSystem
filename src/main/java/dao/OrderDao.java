a09bc854b61c8b67b2baa760d4116af7f99d92bb
SUGAE <setayesh.qsmi@gmail.com>
2022-01-06 20:30:36 +0330

    add saveCOrder in OrderDao

--- src/main/java/dao/OrderDao.java
+++ src/main/java/dao/OrderDao.java
@@ -1,5 +1,19 @@
 package dao;
 
-public class OrderDao {
+import model.orders.Order;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
 
+public class OrderDao {
+    static SessionFactory sessionFactory = new Configuration().configure().buildSessionFactory();
+
+    public void saveCOrder(Order order) {
+        Session session = sessionFactory.openSession();
+        Transaction transaction = session.beginTransaction();
+        session.save(order);
+        transaction.commit();
+        session.close();
+    }
 }
