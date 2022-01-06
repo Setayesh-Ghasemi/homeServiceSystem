2a6b8081dc55b2d242dfdf873ce62d848582711c
SUGAE <setayesh.qsmi@gmail.com>
2022-01-06 14:43:44 +0330

    add Order class in model package

---
+++ src/main/java/model/Order.java
@@ -0,0 +1,22 @@
+package model;
+
+import lombok.Data;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.util.Date;
+
+@Entity
+@Data
+public class Order {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private float cost;
+    private String description;
+    private Date startDate;
+    private Date endDate;
+    private String address;
+}
