dddd68bcd7fc1ae7918db56f1529e31a0fa7891c
SUGAE <setayesh.qsmi@gmail.com>
2022-01-06 21:47:29 +0330

    add ServiceDao class in dao package

---
+++ src/main/java/dao/ServiceDao.java
@@ -0,0 +1,26 @@
+package dao;
+
+import model.services.SubService;
+import org.hibernate.SQLQuery;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+
+public class ServiceDao {
+    static SessionFactory sessionFactory = new Configuration().configure().buildSessionFactory();
+
+    public SubService findServiceByName(String name) {
+        Session session = sessionFactory.openSession();
+        Transaction transaction = session.beginTransaction();
+        String sql = "select * from HomeService where name = :name ";
+        SQLQuery sqlQuery = session.createSQLQuery(sql);
+        sqlQuery.addEntity(SubService.class);
+        sqlQuery.setParameter("name", name);
+        SubService homeServices= (SubService) sqlQuery.list().get(0);
+        session.close();
+        return homeServices;
+    }
+
+
+}
