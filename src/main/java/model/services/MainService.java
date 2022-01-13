22992e82ee0e070588f37e1902ae7edc5f51e8df
SUGAE <setayesh.qsmi@gmail.com>
2022-01-13 09:25:30 +0330

    add MainService class in model

---
+++ src/main/java/model/services/MainService.java
@@ -0,0 +1,25 @@
+package model.services;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class MainService {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    @Column(unique = true)
+    private String name;
+    @OneToMany(mappedBy = "service", fetch = FetchType.EAGER)
+    private Set<SubService> subServices = new HashSet<>();
+}
