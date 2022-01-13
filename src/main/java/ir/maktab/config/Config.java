5a7353677047b27345099818fd074d536e42ee24
SUGAE <setayesh.qsmi@gmail.com>
2022-01-13 21:13:51 +0330

    add Config and DatabaseConfig class

---
+++ src/main/java/ir/maktab/config/Config.java
@@ -0,0 +1,10 @@
+package ir.maktab.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackages = "ir.maktab")
+public class Config {
+
+}
