35e95596b140838d5af43e56aa0aa225ff76f8f9
SUGAE <setayesh.qsmi@gmail.com>
2022-01-13 21:36:29 +0330

    add SubServiceDao class

---
+++ src/main/java/ir/maktab/data/dao/SubServiceDao.java
@@ -0,0 +1,13 @@
+package ir.maktab.data.dao;
+
+import ir.maktab.data.model.entity.services.SubService;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface SubServiceDao extends JpaRepository<SubService, Integer> {
+
+    Optional<SubService> findByName(String name);
+}
