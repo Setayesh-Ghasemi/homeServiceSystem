1ccd196da560e0016b00ebee5a0d1b24315ba2cf
SUGAE <setayesh.qsmi@gmail.com>
2022-01-13 21:34:50 +0330

    add SubServiceDao class

---
+++ src/main/java/ir/maktab/data/model/entity/services/SubService.java
@@ -0,0 +1,25 @@
+package ir.maktab.data.model.entity.services;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class SubService {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    @Column(unique = true)
+    private String name;
+    @ManyToOne
+    private MainService mainService;
+    private long price;
+    private String description;
+}
